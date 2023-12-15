3a2c7f3b863b724955709d6ff902e8e203546c9b
unknown <mdijaz2596@gmail.com>
2023-12-15 19:06:27 +0530

    new change

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
 
-import React,{useState} from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
   const [text, setText] = useState("");
-//   function setText() {
-//     setText(value);
-// }
+  //   function setText() {
+  //     setText(value);
+  // }
   return (
     <div>
       <form action="">
-        <input onChange={(e) => setText(e.target.value)} type="text" placeholder="Input Something" value={Text} />
+        <input onChange={(e) => setText(e.target.value)} type="text" placeholder="Input Something"  />
       </form>
-      <p>Hello { text}!</p>
+      <p>Hello {text}!</p>
     </div>
   )
 }
